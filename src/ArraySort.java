d473ad02c5c939338a8b0914b3d440b98a4dad7a
columbooster <kinglove0403@gmail.com>
2022-09-10 21:21:18 +0900

    Array / sort

---
+++ src/ArraySort.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ArraySort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] nums = new int[3];
+
+        for (int i = 0; i < 3; i++){
+            nums[i] = sc.nextInt();
+        }
+
+        Arrays.sort(nums);
+
+        for (int i = 0; i < 3; i++){
+            System.out.print(nums[i] + " ");
+        }
+
+    }
+}
